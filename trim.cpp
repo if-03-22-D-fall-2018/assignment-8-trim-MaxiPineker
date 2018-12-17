2409e8c031e144a223ee8d2d270252876622eb9a
Maxi Pineker <maxi.pineker@gmail.com>
2018-12-17 22:52:09 +0100

    Made the .h file and the tests are running

--- trim.cpp
+++ trim.cpp
@@ -10,3 +10,16 @@
  * Test functions for trim.h
  * ----------------------------------------------------------
  */
+#include "trim.h"
+#include <string.h>
+
+ void trim(const char *source, char *trimmed_string){
+
+ }
+
+ static void get_start(const char* source, int *begin){
+
+ }
+ static void get_end(const char* source, int *end, int *begin){
+
+ }
--- trim.h
+++ trim.h
@@ -10,3 +10,14 @@
  * Interface for trim
  * ----------------------------------------------------------
  */
+#ifndef TRIM__H
+#define TRIM__H
+
+#define STRLEN 16
+
+ void trim(const char *source, char *trimmed_string);
+
+ static void get_start(const char* source, int *begin);
+ static void get_end(const char* source, int *end, int *begin);
+
+#endif
